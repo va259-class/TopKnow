2aef5721b737d02a7cd33dfb29f2041198f5b491
va259-class <va259@outlook.com>
2026-03-01 11:04:19 +0300

    Branding - Management Client

---
+++ src/backend/hostings/TopKnow.Management.Client/Controllers/UserController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TopKnow.Management.Client.Controllers
+{
+	public class UserController : Controller
+	{
+		public IActionResult Admins()
+		{
+			return View();
+		}
+
+		public IActionResult Players()
+		{
+			return View();
+		}
+
+		public IActionResult Externals()
+		{
+			return View();
+		}
+	}
+}
